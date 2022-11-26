f4b5f6452e668bd643623c192ffbad02a2869866
seraf <serafin.velenc@educa.jcyl.es>
2022-11-27 00:10:24 +0100

    subir clases terminadas invoicePrinting y product

---
+++ src/invoicePrinting.java
@@ -0,0 +1,65 @@
+public class invoicePrinting {
+    private Integer invoiceCode;
+    private String invoiceDate;
+    private String customerCifNif;
+    private String customerName;
+    private String customerAddress;
+    private String customerCity;
+    private String customerProvince;
+
+    public Integer getInvoiceCode() {
+        return invoiceCode;
+    }
+
+    public void setInvoiceCode(Integer invoiceCode) {
+        this.invoiceCode = invoiceCode;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getCustomerCifNif() {
+        return customerCifNif;
+    }
+
+    public void setCustomerCifNif(String customerCifNif) {
+        this.customerCifNif = customerCifNif;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerCity() {
+        return customerCity;
+    }
+
+    public void setCustomerCity(String customerCity) {
+        this.customerCity = customerCity;
+    }
+
+    public String getCustomerProvince() {
+        return customerProvince;
+    }
+
+    public void setCustomerProvince(String customerProvince) {
+        this.customerProvince = customerProvince;
+    }
+}
---
+++ src/product.java
@@ -0,0 +1,56 @@
+public class product {
+    private Integer productCode;
+    private String name;
+    private String mark;
+    private String model;
+    private Integer cost;
+    private Integer vatRate;
+
+    public Integer getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(Integer productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getVatRate() {
+        return vatRate;
+    }
+
+    public void setVatRate(Integer vatRate) {
+        this.vatRate = vatRate;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+}
